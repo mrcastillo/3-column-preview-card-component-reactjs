3dc93c25740370e50a0d4f6d9c2873ae73b1bfb0
Castillo <11235140+mrcastillo@users.noreply.github.com>
2021-07-21 21:49:00 -0400

    Completed Solution
    Completed 3 column preview

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import sedans from "./images/icon-sedans.svg";
+import suvs from "./images/icon-suvs.svg";
+import luxury from "./images/icon-luxury.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+
+      <div className={"column-card-container"}>
+
+        <div className={"column-card-item"} id={"left"}>
+          <div className={"column-card-item-car-pic"}>
+            <img src={sedans} />
+          </div>
+
+          <div className={"column-card-item-text"} >
+            <div className={"column-card-item-h1"} >
+              <h1>SEDANS</h1>
+            </div>
+
+            <div className={"column-card-item-p"}>
+              <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
+            </div>
+          </div>
+          
+          <div className={"column-card-item-button"}>
+            <button>Learn More</button>
+          </div>
+        </div>
+
+        <div className={"column-card-item"} id={"center"}>
+          <div className={"column-card-item-car-pic"}>
+            <img src={suvs} />
+          </div>
+
+          <div className={"column-card-item-text"}>
+            <div className={"column-card-item-h1"}>
+            <h1>SUVS</h1>
+            </div>
+
+            <div className={"column-card-item-p"}>
+              <p>Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.</p>
+            </div>
+          </div>
+          
+
+          <div className={"column-card-item-button"}>
+            <button>Learn More</button>
+          </div>
+        </div>
+
+        <div className={"column-card-item"} id={"right"}>
+          <div className={"column-card-item-car-pic"}>
+            <img src={luxury} />
+          </div>
+
+          <div className={"column-card-item-text"}>
+            <div className={"column-card-item-h1"}>
+              <h1>LUXURY</h1>
+            </div>
+
+            <div className={"column-card-item-p"}>
+              <p>Cruise in the best car brands without the bloated prices. Enjoy the enchanced comfort of a luxury rental and arrive in style.</p>
+            </div>
+          </div>
+          
+
+          <div className={"column-card-item-button"}>
+            <button>Learn More</button>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
